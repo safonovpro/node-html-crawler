b9c3f1166269d4a6291fa53b0512fb6fb7459dd8
Alexey Safonov <alexey@safonov.pro>
2018-06-25 23:38:50 +0300

    Add command line args

--- examples/console-app.js
+++ examples/console-app.js
@@ -1,10 +1,16 @@
+const commandLineArgs = require('command-line-args');
 const Crawler = require('./../classes/crawler');
 
-const protocol = 'http:';
-const domain = 'safonov.pro';
-const crawler = new Crawler({protocol, domain});
+const config = commandLineArgs([
+    {name: 'protocol', alias: 'p', defaultValue: 'http:'},
+    {name: 'domain', alias: 'd', defaultValue: 'example.com'},
+    {name: 'connections', alias: 'c'},
+    {name: 'redirects', alias: 'r'},
+    {name: 'timeout', alias: 't'}
+]);
+const crawler = new Crawler(config);
 
 crawler.crawl();
 crawler.on('data', data => console.log(data.result.statusCode, data.url));
 crawler.on('error', error => console.error(error));
-crawler.on('end', () => console.log(`Finish! All urls on domain ${domain} a crawled!`));
+crawler.on('end', () => console.log(`Finish! All urls on domain ${config.domain} a crawled!`));
