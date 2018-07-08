c80971da1f1f083d11286e62e45ffef7c1d31f21
Alexey Safonov <alexey@safonov.pro>
2018-07-08 14:44:35 +0300

    Simplified example

--- examples/console-app-simple.js
+++ examples/console-app-simple.js
@@ -1,16 +1,9 @@
-const commandLineArgs = require('command-line-args');
 const Crawler = require('../crawler');
 
-const config = commandLineArgs([
-    {name: 'protocol', alias: 'p', defaultValue: 'http:'},
-    {name: 'domain', alias: 'd', defaultValue: 'safonov.pro'},
-    {name: 'connections', alias: 'c'},
-    {name: 'redirects', alias: 'r'},
-    {name: 'timeout', alias: 't'}
-]);
-const crawler = new Crawler(config);
+const domain = process.argv[2];
+const crawler = new Crawler(domain);
 
 crawler.crawl();
 crawler.on('data', data => console.log(data.result.statusCode, data.url));
 crawler.on('error', error => console.error(error));
-crawler.on('end', () => console.log(`Finish! All urls on domain ${config.domain} a crawled!`));
+crawler.on('end', () => console.log(`Finish! All urls on domain ${domain} a crawled!`));
